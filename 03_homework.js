790017abf696464206fde917323f3eb6b717ca61
Florentina Vojvoda <florentina.vojvoda@epitech.eu>
2021-03-16 20:15:00 +0100

    Check if perfect number

--- 03_homework.js
+++ 03_homework.js
@@ -63,4 +63,14 @@
 
 //Detyra5
 
+// function checkIfPerfect(number) {
+//     let sum = 0;
+//     for (let i = 1; i < number; i++) {
+//         if (number % i == 0) {
+//             sum = sum + i;
+//         }
+//     }
+//     return sum == number ? true : false;
+// }
 
+// console.log(checkIfPerfect(parseInt(prompt("Write a number:"))));
